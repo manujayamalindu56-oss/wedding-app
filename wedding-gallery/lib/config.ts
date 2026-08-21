d07906bd33ab7e8090421906214c0d1cdd5c7949
manujayamalindu56-oss <manujayamalindu56@gmail.com>
2026-08-21 21:29:28 +0530

    update all features new

--- wedding-gallery/lib/config.ts
+++ wedding-gallery/lib/config.ts
@@ -4,7 +4,7 @@ export const AppConfig = {
   coupleNames: "චමෝද්  & මෙනුෂා", 
   hostName: "Mr & Mrs", 
   welcomeMessage: "අපේ ජීවිතයේ සුන්දරම දවසට සම්බන්ධ වීම ගැන ඔබට ගොඩක් ස්තුතියි!",
-  weddingDate: "2026-10-21", 
+  weddingDate: "2026-10-30", 
   adminPassword: "123", 
   // Slideshow එකට පසුබිමින් වාදනය වන සංගීත ලින්ක් එක
   backgroundMusicUrl: "https://www.bensound.com/bensound-music/bensound-tenderness.mp3",
